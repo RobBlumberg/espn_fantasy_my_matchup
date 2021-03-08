2a66c9ce069cd53609c1c3e8aceabd9ae8a86a97
RobBlumberg <robertblumberg97@gmail.com>
2021-03-08 13:21:12 -0500

    add logging

--- src/handler.py
+++ src/handler.py
@@ -1,9 +1,12 @@
 from espn_fantasy_matchup_stats.auth import my_league
 from espn_fantasy_matchup_stats.my_team import MyTeam
+import logging
+
+logging.basicConfig(level = logging.INFO)
 
 # Find my team and opponent's team
 my_team = MyTeam(my_league, "Drip Bayless")
 
 # Get stats
 comparison = my_team.get_current_matchup_comparison()
-print(comparison)
+logging.info(comparison)
