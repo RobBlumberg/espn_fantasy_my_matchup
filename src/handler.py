efe84a2199b2797db537451c44354dc2a93479fe
RobBlumberg <robertblumberg97@gmail.com>
2021-04-10 18:58:05 -0400

    set up handler function

--- src/handler.py
+++ src/handler.py
@@ -8,5 +8,8 @@ logging.basicConfig(level = logging.INFO)
 my_team = MyTeam(my_league, "Drip Bayless")
 
 # Get stats
-comparison = my_team.get_current_matchup_comparison()
-logging.info(comparison)
+def handle(event, context):
+    comparison = my_team.get_current_matchup_comparison()
+    logging.info(comparison)
+
+    return comparison
