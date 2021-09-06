3d4824c15eeca813d7453a00ba2c3c6648c36163
RobBlumberg <robertblumberg97@gmail.com>
2021-09-06 14:04:16 -0400

    add write to handler

--- espn_fantasy_my_matchup/handler.py
+++ espn_fantasy_my_matchup/handler.py
@@ -1,15 +1,31 @@
 from espn_fantasy_matchup_stats.auth import my_league
 from espn_fantasy_matchup_stats.my_team import MyTeam
-import logging
+from .helper_io import fantasy_comparison_response_transformer
+import boto3
 import os
+import logging
 
 logging.basicConfig(level = logging.INFO)
 
-# Get stats
+
+def write_outputs(payload):
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('espn_my_fantasy_outputs')
+    table.put_item(Item=payload)
+
+
 def handle(event, context):
-        
+    
+    # get stats
     my_team = MyTeam(my_league, "Drip Bayless")
     comparison = my_team.get_current_matchup_comparison()
-    logging.info(f"\n{comparison}")
-
-    return comparison.to_json()
+    logging.info(f"\n{comparison.to_json()}")
+    
+    # transform payload and write to dynamoDB
+    payload = fantasy_comparison_response_transformer(comparison)
+    if not os.environ.get("DEBUG"):
+        logging.info("posted")
+        write_outputs(payload)
+    else:
+        logging.info("not posted")
