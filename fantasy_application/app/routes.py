8ea26be173387ad49a7f8286f885959a0863056e
RobBlumberg <robertblumberg97@gmail.com>
2021-12-30 19:09:04 -0500

    accept user input

--- fantasy_application/app/routes.py
+++ fantasy_application/app/routes.py
@@ -1,14 +1,12 @@
-from datetime import date
+from datetime import datetime
 
 from espn_fantasy_matchup_stats.fantasy import MyTeam
 from espn_fantasy_matchup_stats.fantasy.league_auth import league_from_env
 from flask import render_template
+from flask import request
 
 from app import app
 from app.forms import MatchupForm
-
-START_DATE = date(2021, 12, 29)
-END_DATE = date(2022, 1, 2)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -16,19 +14,33 @@ END_DATE = date(2022, 1, 2)
 def index():
     form = MatchupForm()
 
-    my_league = league_from_env()
-    team1 = MyTeam(my_league, "Drip Bayless")
-    team2 = team1.get_opponents_team()
-    comparison = MyTeam.matchup_comparison(
-        team1, team2, START_DATE, END_DATE, stat_type="total"
-    )
-    decimals = 2
-    items = [
-        {
-            "stat": k,
-            "my_score": round(v[0], decimals),
-            "opp_score": round(v[1], decimals),
-        }
-        for k, v in comparison.T.to_dict("list").items()
-    ]
+    if form.validate_on_submit():
+        team1_name = form.your_team.data
+        team2_name = form.opp_team.data
+        stat_type = form.stat_type.data
+        start_date = form.start_date.data
+        end_date = form.end_date.data
+
+        my_league = league_from_env()
+        team1 = MyTeam(my_league, team1_name)
+        team2 = MyTeam(my_league, team2_name)
+
+        start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+        end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+
+        comparison = MyTeam.matchup_comparison(
+            team1, team2, start_date, end_date, stat_type=stat_type
+        )
+        decimals = 2
+        items = [
+            {
+                "stat": k,
+                "my_score": round(v[0], decimals),
+                "opp_score": round(v[1], decimals),
+            }
+            for k, v in comparison.T.to_dict("list").items()
+        ]
+    elif request.method == "GET":
+        items = [{}]
+
     return render_template("index.html", form=form, items=items)
