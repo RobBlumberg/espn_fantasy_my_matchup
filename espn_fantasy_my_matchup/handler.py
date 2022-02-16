23657f99231fcfe87049e3ed5f3ba0f7b03daeaf
RobBlumberg <robertblumberg97@gmail.com>
2022-02-16 18:32:41 -0500

    wrap handler in metaflow flow

--- espn_fantasy_my_matchup/handler.py
+++ espn_fantasy_my_matchup/handler.py
@@ -6,35 +6,54 @@ import boto3
 from espn_fantasy_matchup_stats.fantasy import MyTeam
 from espn_fantasy_matchup_stats.fantasy.league_auth import league_from_env
 
-from .helper_io import fantasy_comparison_response_transformer
+from espn_fantasy_my_matchup.helper_io import fantasy_comparison_response_transformer
 
 logging.basicConfig(level=logging.INFO)
 
+from metaflow import FlowSpec, step, metadata, conda
+
+from datetime import datetime
+import pytz
 
 START_DATE = date(2021, 12, 28)
 END_DATE = date(2021, 12, 29)
 
+class ESPNFantasyFlow(FlowSpec):
 
-def write_outputs(payload):
+    @step
+    def start(self):
+        print("Starting metaflow workflow...")
 
-    dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table("espn_my_fantasy_outputs")
-    table.put_item(Item=payload)
+        # get stats
+        my_league = league_from_env()
+        self.my_team = MyTeam(my_league, "Drip Bayless")
+        self.opp_team = self.my_team.get_opponents_team()
+        
+        self.next(self.get_comparison)
 
+    @step
+    def get_comparison(self):
+
+        self.comparison = self.my_team.matchup_comparison(
+            self.my_team, self.opp_team, START_DATE, END_DATE
+        ).to_dict()
+        logging.info(f"\n{self.comparison}")
+
+        self.next(self.write_outputs)
+
+    @step
+    def write_outputs(self):
+        payload = fantasy_comparison_response_transformer(self.comparison)
+
+        self.next(self.end)
+    
+    @step
+    def end(self):
+        pass
 
 def handle(event, context):
 
-    # get stats
-    my_league = league_from_env()
-    my_team = MyTeam(my_league, "Drip Bayless")
-    opp_team = my_team.get_opponents_team()
-    comparison = my_team.matchup_comparison(
-        my_team, opp_team, START_DATE, END_DATE
-    ).to_dict()
-    logging.info(f"\n{comparison}")
+    ESPNFantasyFlow()
 
-    # transform payload and write to dynamoDB
-    payload = fantasy_comparison_response_transformer(comparison)
-    if not os.environ.get("DEBUG"):
-        write_outputs(payload)
-        logging.info("Posted outputs")
+if __name__ == "__main__":
+    handle({}, {})
