003884bf7d39138a13b636f2dcb562b18cc9b3eb
RobBlumberg <robertblumberg97@gmail.com>
2022-02-16 18:30:38 -0500

    add ecr resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,47 @@
-provider "aws" {
-    region = "us-east-1"
-    profile = "default"
-}
-
 locals {
   my_league_creds = jsondecode(data.aws_secretsmanager_secret_version.league_creds.secret_string)
 }
 
 data "aws_secretsmanager_secret_version" "league_creds" {
-    secret_id = "arn:aws:secretsmanager:us-east-1:978072805127:secret:espn-fantasy-league-creds-IKFhtX"
+    secret_id = "arn:aws:secretsmanager:us-west-2:978072805127:secret:espn-fantasy-league-creds-IKFhtX"
+}
+
+resource "aws_ecr_repository" "repository" {
+  name                 = "espn_my_matchup"
+  image_tag_mutability = "MUTABLE"
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "null_resource" "ecr_image" {
+  depends_on = [
+    aws_ecr_repository.repository
+  ]
+
+  triggers = {
+    image_version = var.image_version
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      cd ..
+      make push_image
+    EOF
+  }
+}
+
+data "aws_ecr_image" "lambda_image" {
+  depends_on = [
+    null_resource.ecr_image
+  ]
+  repository_name = aws_ecr_repository.repository.name
+  image_tag       = var.image_version
 }
 
 resource "aws_iam_role_policy" "lambda_policy" {
@@ -27,7 +60,7 @@ resource "aws_lambda_function" "espn_my_matchup" {
   role                   = aws_iam_role.lambda_role.arn
   memory_size            = 1024
   timeout                = 60
-  image_uri              = "978072805127.dkr.ecr.us-east-1.amazonaws.com/espn_my_matchup:latest"
+  image_uri              = "${aws_ecr_repository.repository.repository_url}@${data.aws_ecr_image.lambda_image.id}"
   package_type           = "Image"
   environment {
     variables = {
