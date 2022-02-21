2be305a255f1128e8e052f512252436a10f6b337
RobBlumberg <robertblumberg97@gmail.com>
2022-02-21 17:30:40 -0500

    define metaflow infra first

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,89 @@
+### METAFLOW INFRA ###
+
+# Create VPC with 3 private/public subnets into which services will de deployed
+# and enable NAT gateway for internet connectivity
+module "vpc" {
+
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "metaflow"
+  cidr = "10.100.0.0/16"
+
+  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  private_subnets = ["10.100.1.0/24", "10.100.2.0/24", "10.100.3.0/24"]
+  public_subnets  = ["10.100.101.0/24", "10.100.102.0/24", "10.100.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+}
+
+module "metaflow-computation" {
+  source = "github.com/outerbounds/terraform-aws-metaflow/modules/computation"
+
+  resource_prefix = local.resource_prefix
+  resource_suffix = local.resource_suffix
+
+  batch_type                              = "ec2"
+  compute_environment_desired_vcpus       = var.compute_environment_desired_vcpus
+  compute_environment_instance_types      = var.compute_environment_instance_types
+  compute_environment_max_vcpus           = var.compute_environment_max_vcpus
+  compute_environment_min_vcpus           = var.compute_environment_min_vcpus
+  enable_step_functions                   = var.enable_step_functions
+  iam_partition                           = "aws"
+  metaflow_step_functions_dynamodb_policy = ""
+  subnet1_id                              = element(module.vpc.private_subnets, 0)
+  subnet2_id                              = element(module.vpc.private_subnets, 1)
+  metaflow_vpc_id                         = module.vpc.vpc_id
+
+  standard_tags = {
+    Terraformed = true
+  }
+}
+
+module "metaflow-metadata-service" {
+  source = "github.com/outerbounds/terraform-aws-metaflow/modules/metadata-service"
+
+  resource_prefix = local.resource_prefix
+  resource_suffix = local.resource_suffix
+
+  access_list_cidr_blocks          = var.access_list_cidr_blocks
+  database_password                = module.metaflow-datastore.database_password
+  database_username                = module.metaflow-datastore.database_username
+  datastore_s3_bucket_kms_key_arn  = module.metaflow-datastore.datastore_s3_bucket_kms_key_arn
+  fargate_execution_role_arn       = module.metaflow-computation.ecs_execution_role_arn
+  iam_partition                    = var.iam_partition
+  metadata_service_container_image = local.metadata_service_container_image
+  metaflow_vpc_id                  = module.vpc.vpc_id
+  rds_master_instance_endpoint     = module.metaflow-datastore.rds_master_instance_endpoint
+  s3_bucket_arn                    = module.metaflow-datastore.s3_bucket_arn
+  subnet1_id                       = element(module.vpc.private_subnets, 0)
+  subnet2_id                       = element(module.vpc.private_subnets, 1)
+  vpc_cidr_block                   = module.vpc.vpc_cidr_block
+
+  standard_tags = {
+    Terraformed = true
+  }
+}
+
+module "metaflow-datastore" {
+  source = "github.com/outerbounds/terraform-aws-metaflow/modules/datastore"
+
+  resource_prefix = local.resource_prefix
+  resource_suffix = local.resource_suffix
+
+  ecs_execution_role_arn             = module.metaflow-computation.ecs_execution_role_arn
+  ecs_instance_role_arn              = module.metaflow-computation.ecs_instance_role_arn
+  metadata_service_security_group_id = module.metaflow-metadata-service.metadata_service_security_group_id
+  subnet1_id                         = element(module.vpc.private_subnets, 0)
+  subnet2_id                         = element(module.vpc.private_subnets, 1)
+  metaflow_vpc_id                    = module.vpc.vpc_id
+
+  standard_tags = {
+    Terraformed = true
+  }
+}
+
 ### ECR LAMBDA FUNCTION ###
 
 # get league secrets from secret manager
@@ -75,90 +161,10 @@ resource "aws_lambda_function" "espn_my_matchup" {
       LEAGUE_ID      = local.my_league_creds.LEAGUE_ID
     }
   }
+  # vpc_config {
+  #   subnet_ids         = [element(module.vpc.private_subnets, 0).id]
+  #   security_group_ids = [aws_default_security_group.default_security_group.id]
+  # }
 }
 
 
-### METAFLOW INFRA ###
-
-# Create VPC into which services will de deployed
-module "vpc" {
-
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "metaflow"
-  cidr = "10.100.0.0/16"
-
-  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  private_subnets = ["10.100.1.0/24", "10.100.2.0/24", "10.100.3.0/24"]
-  public_subnets  = ["10.100.101.0/24", "10.100.102.0/24", "10.100.103.0/24"]
-
-  enable_nat_gateway = false
-  single_nat_gateway = true
-
-}
-
-module "metaflow-computation" {
-  source = "github.com/outerbounds/terraform-aws-metaflow/modules/computation"
-
-  resource_prefix = local.resource_prefix
-  resource_suffix = local.resource_suffix
-
-  batch_type                              = "ec2"
-  compute_environment_desired_vcpus       = var.compute_environment_desired_vcpus
-  compute_environment_instance_types      = var.compute_environment_instance_types
-  compute_environment_max_vcpus           = var.compute_environment_max_vcpus
-  compute_environment_min_vcpus           = var.compute_environment_min_vcpus
-  enable_step_functions                   = var.enable_step_functions
-  iam_partition                           = "aws"
-  metaflow_step_functions_dynamodb_policy = ""
-  subnet1_id                              = element(module.vpc.private_subnets, 0)
-  subnet2_id                              = element(module.vpc.private_subnets, 1)
-  metaflow_vpc_id                         = module.vpc.vpc_id
-
-  standard_tags = {
-    Terraformed = true
-  }
-}
-
-module "metaflow-metadata-service" {
-  source = "github.com/outerbounds/terraform-aws-metaflow/modules/metadata-service"
-
-  resource_prefix = local.resource_prefix
-  resource_suffix = local.resource_suffix
-
-  access_list_cidr_blocks          = var.access_list_cidr_blocks
-  database_password                = module.metaflow-datastore.database_password
-  database_username                = module.metaflow-datastore.database_username
-  datastore_s3_bucket_kms_key_arn  = module.metaflow-datastore.datastore_s3_bucket_kms_key_arn
-  fargate_execution_role_arn       = module.metaflow-computation.ecs_execution_role_arn
-  iam_partition                    = var.iam_partition
-  metadata_service_container_image = local.metadata_service_container_image
-  metaflow_vpc_id                  = module.vpc.vpc_id
-  rds_master_instance_endpoint     = module.metaflow-datastore.rds_master_instance_endpoint
-  s3_bucket_arn                    = module.metaflow-datastore.s3_bucket_arn
-  subnet1_id                       = element(module.vpc.private_subnets, 0)
-  subnet2_id                       = element(module.vpc.private_subnets, 1)
-  vpc_cidr_block                   = module.vpc.vpc_cidr_block
-
-  standard_tags = {
-    Terraformed = true
-  }
-}
-
-module "metaflow-datastore" {
-  source = "github.com/outerbounds/terraform-aws-metaflow/modules/datastore"
-
-  resource_prefix = local.resource_prefix
-  resource_suffix = local.resource_suffix
-
-  ecs_execution_role_arn             = module.metaflow-computation.ecs_execution_role_arn
-  ecs_instance_role_arn              = module.metaflow-computation.ecs_instance_role_arn
-  metadata_service_security_group_id = module.metaflow-metadata-service.metadata_service_security_group_id
-  subnet1_id                         = element(module.vpc.private_subnets, 0)
-  subnet2_id                         = element(module.vpc.private_subnets, 1)
-  metaflow_vpc_id                    = module.vpc.vpc_id
-
-  standard_tags = {
-    Terraformed = true
-  }
-}
